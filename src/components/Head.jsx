71f601fd756f5fdcf8359c545781589c83f0cd93
Pugsy <nimsarakarunaratne@gmail.com>
2026-02-09 10:16:06 +0530

    menu building

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,26 +3,31 @@ import mypic from '../assets/startpic.png'
 
 function Head() {
   return (
-    <div className=" text-gray-500 flex flex-row w-screen h-screen ">
+    <div className=" text-gray-400 flex flex-row w-screen h-screen ">
         <img src={mypic} alt="My Picture" className='h-full w-1/2 object-cover' />
         <div className="bg-black flex-1 ">
-            <h1 className='text-9xl font-bold'>Nimsara</h1>
-            <h1 className='text-7xl font-bold '>Karunaratne</h1>
-            <div className=" text-gray-500">
-                <ul>
-                    About Me
+            <h1 className='text-9xl font-bold px-6 py-3'>Nimsara</h1>
+            <hr className="border-gray-400 mx-6 pb-1"/>
+            <h1 className='text-7xl font-bold pl-50 pb-3 '>Karunaratne</h1>
+            <div className=" text-gray-500 py-4 font-light text-4xl ">
+                <ul className='py-3 px-6'>
+                    <button className="hover:text-gray-300" >About Me</button>
+                    <span className="absolute left-1/2 -translate-x-1/2 top-full mt-2
+    hidden group-hover:block
+    bg-black text-white text-sm px-3 py-1 rounded
+    whitespace-nowrap">labi labi labi</span>
                 </ul>
-                <ul>
-                    Skills
+                <ul className='py-3 px-6'>
+                    <button className="hover:text-gray-300">Skills</button>
                 </ul>
-                <ul>
-                    Projects
+                <ul className='py-3 px-6'>
+                    <button className="hover:text-gray-300">Projects</button>
                 </ul>
-                <ul>
-                    Experience
+                <ul className='py-3 px-6'>
+                    <button className="hover:text-gray-300">Experience</button>
                 </ul>       
-                <ul>
-                    Contact
+                <ul className='py-3 px-6'>
+                     <button className="hover:text-gray-300">Contact</button>
                 </ul>
             </div>
         </div>
